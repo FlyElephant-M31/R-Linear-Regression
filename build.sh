72c903899bb61ead780395660902be2fb66992f4
Dmitry Spodarets <d.spodarets@flyelephant.net>
2015-12-26 09:25:05 +0200

    fix

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #sudo apt-get update | tee -a build.log
 #sudo apt-get -y install r-base r-base-dev | tee -a build.log
-sudo dpkg --get-selections
+uname -a  | tee -a build.log
+sudo dpkg --get-selections  | tee -a build.log
 r --version | tee -a build.log
 rscript --version | tee -a build.log
 R --version | tee -a build.log
