2c7fd3445fc93ce16714273447754c14a42cd56c
Dmitry Spodarets <d.spodarets@flyelephant.net>
2015-12-26 03:23:56 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-sudo apt-get update | tee -a result.dat
-sudo apt-get -y install r-base r-base-dev | tee -a result.dat
-pwd | tee -a result.dat
-R --version | tee -a result.dat
-Rscript --version | tee -a result.dat
-uname -a | tee -a result.dat
-Rscript LR.R | tee -a result.dat
+Rscript LR.R
