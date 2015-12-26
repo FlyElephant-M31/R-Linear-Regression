587ad797aee21830c2d9f85b55bb0d7db729fdae
Dmitry Spodarets <d.spodarets@flyelephant.net>
2015-12-26 02:57:11 +0200

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sudo apt-get update | tee -a result.dat
+sudo apt-get -y install r-base r-base-dev | tee -a result.dat
 pwd | tee -a result.dat
 R --version | tee -a result.dat
 Rscript --version | tee -a result.dat
